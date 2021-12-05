0f04833caa47936ea10c94eecb6cbcd35f3b29f6
teo09 <teo.obando09@gmail.com>
2021-12-05 02:24:02 -0600

    Creando form y pasandolo como context hacia la vista index

---
+++ src/boletin/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+#Creando nuestros formularios
+class Reg_Form(forms.Form):
+    nombre = forms.CharField(max_length=100)
+    edad = forms.IntegerField()
--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,6 +1,12 @@
+from django import forms
 from django.shortcuts import render
+from .forms import Reg_Form
 
 # Create your views here.
 
 def index(request): 
-    return render(request, 'index.html', {})
+    form = Reg_Form()
+    context = {
+        'formm' : form,
+    }
+    return render(request, 'index.html', context)
