ad6b3d2552dc6285db8f559d236f14a748d0707c
teo09 <teo.obando09@gmail.com>
2021-12-05 15:27:55 -0600

    Creando form con el modelo, para ser utilizado y personalizado en la administración de django

--- src/boletin/admin.py
+++ src/boletin/admin.py
@@ -3,13 +3,15 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Registrado
+from .forms import Regis_model_form
 
 class AdminRegistrado(admin.ModelAdmin): 
     list_display = ["__str__", "nombre", "timestamp"]
+    form = Regis_model_form
     list_filter = ["timestamp"]
     list_editable = ["nombre"]
     search_fields = ["email", "nombre"]
-    class Meta: 
-        model = Registrado
+    #class Meta: 
+    #    model = Registrado
 
 admin.site.register(Registrado, AdminRegistrado)
--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.forms import fields
+from .models import Registrado
 
 #Creando nuestros formularios
 class Reg_Form(forms.Form):
     nombre = forms.CharField(max_length=100)
     email = forms.EmailField()
+    
+#Creando formulario con nuestro modelo
+class Regis_model_form(forms.ModelForm):
+    class Meta:
+        model = Registrado
+        fields = ['nombre', 'email']
