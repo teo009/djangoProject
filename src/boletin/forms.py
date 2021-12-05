76c11b52af99979a3f8c4544fecdf3acaee6fb60
teo09 <teo.obando09@gmail.com>
2021-12-05 15:50:50 -0600

    Validacion de forms con el modelo, usados en la administración de django

--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -12,3 +12,18 @@ class Regis_model_form(forms.ModelForm):
     class Meta:
         model = Registrado
         fields = ['nombre', 'email']
+        
+    #Creando nuestras propias validaciones
+    def clean_email(self):
+        email_field = self.cleaned_data.get('email')
+        #Validación .edu en los emails
+        email_base, email_provider = email_field.split('@')
+        email_domain, email_extension = email_provider.split('.')
+        if not email_extension == 'edu':
+            raise forms.ValidationError('Por favor, utiliza un correo con la extención ".edu"')
+        return email_field
+    
+    def clean_nombre(self):
+        nombre_field = self.cleaned_data.get('nombre')
+        #Validaciones
+        return nombre_field
