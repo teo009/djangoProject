776e374ea9a76e0ec7ac56f0345b3798a1a86e7f
teo09 <teo.obando09@gmail.com>
2021-12-07 16:34:03 -0600

    Usando el form del modelo en la vista, validando campos vacíos con info por defecto y modificando el context

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.shortcuts import render
-from .forms import Reg_Form
+from .forms import Reg_Form, Regis_model_form
 from .models import Registrado
 
 # Create your views here.
@@ -9,19 +9,32 @@ def index(request):
     titulo = 'Hi there'
     if request.user.is_authenticated:
         titulo = 'Bienvenido %s' %(request.user)
+    form = Regis_model_form(request.POST or None)
     
-    form = Reg_Form(request.POST or None)
-    if form.is_valid():
-        form_data = form.cleaned_data
-        form_name = (form_data.get("nombre"))
-        form_email = (form_data.get("email"))
-        #Guardar en el modelo
-        save_data = Registrado.objects.create(
-            nombre=form_name,
-            email=form_email,
-        )
     context = {
         'titulo' : titulo,
         'formm' : form,
     }
+    
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if not instance.nombre:
+            instance.nombre = 'Persona'
+        instance.save()
+        
+        context = {
+            'titulo': 'Gracias %s!' %(instance.nombre)
+        }
+        
+        print(instance)
+        print(instance.timestamp)
+        
+        #form_data = form.cleaned_data
+        #form_name = (form_data.get("nombre"))
+        #form_email = (form_data.get("email"))
+        #Guardar en el modelo
+        #save_data = Registrado.objects.create(
+        #    nombre=form_name,
+        #    email=form_email,
+        #)
     return render(request, 'index.html', context)
